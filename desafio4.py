45735c8b06704d3dd8a103f3ef3b026fb29ec27a
KauanViniciusRibeiro <viniciuskauan805@gmail.com>
2024-09-03 11:43:12 -0300

    'Kauan'

---
+++ desafio4.py
@@ -0,0 +1,20 @@
+print('=======================================================================')
+print('')
+
+# Declarando as variaveis.
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outro numero: '))
+
+print('')
+
+# Declarando se os numeros são iguais.
+if num1 == num2:
+  print('Os numeros são iguais.')
+# Declarando qual o numero é maior.
+if num1 > num2:
+  print(f'O numero {num1} é maior que o numero {num2}.')
+if num1 < num2:
+  print(f'O numero {num1} é menor que o numero {num2}.')
+
+print('')
+print('=======================================================================')
